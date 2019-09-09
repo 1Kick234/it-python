5f1fd2c5320c78e1bdaaef469690fe235a54cde8
Gavin <gavin_is_1kick234@yahoo.com>
2019-09-09 10:46:25 -0400

    Changes #s to |s

--- Test2.py
+++ Test2.py
@@ -2,7 +2,7 @@ def banner(subject, author):
     byline = f"By {author}"
     banner_length = max(len(subject), len(byline)) + 4
     print("=" * banner_length)
-    print(f"#{subject:^{banner_length-2}}#")
-    print(f"#{byline:^{banner_length-2}}#")
+    print(f"|{subject:^{banner_length-2}}|")
+    print(f"|{byline:^{banner_length-2}}|")
     print("=" * banner_length)
 banner("Time Machine", "Rocket Man")

ea0f9f2ac22c8d8705c94fed78d1ace773bbe9d5
Gavin <gavin_is_1kick234@yahoo.com>
2019-09-10 10:14:01 -0400

    Intial commit

---
+++ Functions.py
@@ -0,0 +1,10 @@
+prompt = input("Are you sure you want to do this? >")
+def actions(user_input):
+    global action
+    global yes_or_no
+    if user_input == "yes" or "y" or "ye":
+        yes_or_no = 1
+    elif user_input == "no" "n":
+        print("oh ok")
+
+actions(prompt)

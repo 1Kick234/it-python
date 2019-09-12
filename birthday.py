40ffd1beff556f79028682040bea4536914ae7a8
Gavin <gavin_is_1kick234@yahoo.com>
2019-09-12 10:11:16 -0400

    Gets brithdate

--- birthday.py
+++ birthday.py
@@ -1,3 +1,29 @@
 from banner import banner
+import datetime
 
 banner("Birthday", "Gavin")
+
+# 1. Find out when you were born
+# 2. Figure out how many days until your birthday
+# 3. Print information about the birthday: Days until, Days ago, or Happy birthday
+
+def main():
+    get_birthday_info()
+    compute_days_between_dates()
+    print_birthday_info()
+
+def get_birhtday_info():
+    print("When were you born?")
+    year = int(input("Year [YYYY] > "))
+    month = int(input("Month [MM] > "))
+    day = int(input("Day [DD] > "))
+
+    birthday = datetime.date(year, month, day)
+    return birthday
+
+def compute_days_between_dates():
+    pass
+
+def print_birthday_info():
+    pass
+get_birhtday_info()
